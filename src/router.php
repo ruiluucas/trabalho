ddc6b35afd7d56a59e8c08599b4449ab34e06f31
ruiluucas <lucasantony815@gmail.com>
2026-03-05 21:37:32 -0300

    hotfix: resolve empty response

--- src/router.php
+++ src/router.php
@@ -1,27 +1,26 @@
 <?php
 
-declare(strict_types=1);
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$path = $request->getPathInfo();
+
+$response = new Response();
 
 $routes = [
     '/hello' => __DIR__ . '/controller/hello.php',
     '/bye'   => __DIR__ . '/controller/bye.php',
 ];
 
-if (array_key_exists($path, $routes)) {
-    $response = require $routes[$path];
+$path = $request->getPathInfo();
+
+if (isset($routes[$path])) {
+    require $routes[$path];
 } else {
-    $response = new Response(
-        '<h1>Erro 404</h1><p>Página não encontrada</p>',
-        Response::HTTP_NOT_FOUND
-    );
+    $response->setStatusCode(404);
+    $response->setContent('Not Found');
 }
 
 $response->send();
